7efae22e4b5880b23d3e0e9a20455af7ab44f6c9
dimavolo1 <125883080+dimavolo1@users.noreply.github.com>
2023-11-07 14:27:49 +0500

    commit init

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,13 @@ import java.io.IOException;
 public class Main {
     private static final int SERVER_SOCKET = 9999;
     private static final int poolSizeThreads = 64;
+
     public static void main(String[] args) throws InterruptedException {
 
         Server server = new Server(SERVER_SOCKET, poolSizeThreads);
         server.addHandler("GET", "/messages", ((request, responseStream) -> {
             try {
-                server.responseWithoutContent(responseStream,"404", "Not found");
+                server.responseWithoutContent(responseStream, "404", "Not found");
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
